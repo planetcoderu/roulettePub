b929fc0463faccb4a123f6ecb83b24e85c0b19bf
Adil Sagengaliyev <saginst@mail.ru>
2024-04-09 23:23:35 +0500

    initial commit

---
+++ src/main/java/com/moneymaker/MoneymakerApplication.java
@@ -0,0 +1,13 @@
+package com.moneymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moneymaker/MoneymakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
